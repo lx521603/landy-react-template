b23317d4d6d7f07ac1ef84a19957f7ab79c0529e
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-09-30 00:51:22 +0800

    Update index.tsx

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -2,14 +2,20 @@ import { Row, Col } from "antd";
 import { withTranslation } from "react-i18next";
 import { Slide } from "react-awesome-reveal";
 import { ContactProps } from "./types";
-import Block from "./Block";
+import Block from "../Block";
 import { ContactContainer } from "./styles";
 
 const Contact = ({ title, content, id, t }: ContactProps) => {
   return (
     <ContactContainer id={id}>
       <Row justify="center" align="middle">
-        <Col lg={12} md={16} sm={24} xs={24}>
+        <Col
+          lg={16}
+          md={18}
+          sm={22}
+          xs={24}
+          style={{ wordWrap: "break-word", overflow: "visible" }}
+        >
           <Slide direction="left" triggerOnce>
             <Block title={title} content={content} />
           </Slide>
@@ -20,4 +26,3 @@ const Contact = ({ title, content, id, t }: ContactProps) => {
 };
 
 export default withTranslation()(Contact);
-
